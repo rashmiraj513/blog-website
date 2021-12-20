601892fae98c0f90bbe94404dd1a74e498e7b661
Rashmi Raj <rashmiraj7877@gmail.com>
2021-12-21 02:33:15 +0530

    almost ready

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 const homeStartingContent = "Welcome to this blog website. Here, you can log the events which happened with you and after some time you can read them! It's like you are maintaining your online diary which can be accessed from anywhere and no need to carry that. It's in your phone and just a click away. This is just your technical diary which can be accessed from any device. So, start blogging your daily events now, so that you can remember your events and some days later you can enjoy those memories or you can at least remember them and be happy and at last WELCOME AGAIN!";
-const aboutContent = "This is a blogging website. Here, you can write anything that happened with you at any time with a day stamp and you can access them later. Now, you can write those memories in going in Compose section. Since this website is just one click away from you so that you can access them from anywhere across the globe. So, start writing today from this time. You can write your memories while traveling or doing anything which time you can type something. This website is very user-friendly and has a very simple UI/UX design. The design is so simple even a child can use this. Hence, start blogging your thoughts or ideas or your past today.";
-const contactContent = "Since this website is made for only fun purposes. Hence, you can think you are the OWNER of this website. Just keep enjoying and keep blogging your memories so that later time, you can read them, find them, and re-live them.";
 
 const db = require("./connections/config");
 const schemas = require("./models/model");
@@ -45,8 +43,10 @@ app.post("/compose", function(req, res) {
     });
 });
 
+let requestedPostId;
+
 app.get("/posts/:postId", function(req, res) {
-    const requestedPostId = req.params.postId;
+    requestedPostId = req.params.postId;
     Post.findOne({_id: requestedPostId}, function(err, post) {
         if(err) {
             throw err;
@@ -57,15 +57,56 @@ app.get("/posts/:postId", function(req, res) {
 });
 
 app.get("/about", function(req, res) {
-    res.render("about", {aboutCont: aboutContent})
+    res.render("about")
 });
 
 app.get("/contact", function(req, res) {
-    res.render("contact", {contactCont: contactContent})
+    res.render("contact")
 });
 
 app.get("/compose", function(req, res) {
     res.render("compose")
+});
+
+app.get("/delete", function(req, res) {
+    Post.deleteOne({_id: requestedPostId}, function(err, post) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    })
+});
+
+app.get("/update", function(req, res) {
+    Post.find({_id: requestedPostId}, function(err, post) {
+        if(err) {
+            console.log(err);
+        } else {
+            const element = post[0];
+            res.render("update", {title: element.title, content: element.content});
+        }
+    });
+});
+
+app.post("/update", function(req, res) {
+    const newTitle = req.body.updatedTitle;
+    const newContent = req.body.updatedBody;
+    Post.find({_id: requestedPostId}, function(err, post) {
+        if(err) {
+            console.log(err);
+        } else {
+            post[0].title = newTitle;
+            post[0].content = newContent;
+            post[0].save(function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/");
+                }
+            });
+        }
+    });
 });
 
 const PORT = process.env.PORT || 4000;
--- connections/config.js
+++ connections/config.js
@@ -7,9 +7,9 @@ const db = process.env.DATABASE;
 const user = process.env.USER;
 const clusterURL = process.env.URL;
 
-// const uri = "mongodb://localhost:27017/blogWebsite";
+const uri = "mongodb://localhost:27017/blogWebsite";
 
-const uri = "mongodb+srv://" + user + ":" + password + "@" + clusterURL + db + "?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://" + user + ":" + password + "@" + clusterURL + db + "?retryWrites=true&w=majority";
 
 // connecting to the database...
 mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
